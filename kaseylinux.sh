436148a6875be50516a9508f9e740edf6bd1feb4
yesak123 <klitchford044@g.rwu.edu>
2021-01-12 00:16:30 -0500

    Final push

--- kaseylinux.sh
+++ kaseylinux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #echo tty1 > /etc/securetty
 #getent groups root, wheel, adm, admin
-#I know this isn't prime. It's actually quite terrible, and I want it to be much better. 
+#I know this isn't prime. It's actually quite terrible, and I want it to be much better.
+#https://github.com/ucrcyber/CCDC ?
+#cron
 ###Kasey Litchford - 01/11/2021
 
 while [[ $# -ne 0 ]]; do
@@ -22,15 +24,15 @@ while [[ $# -ne 0 ]]; do
 		--sums						#makes sums of directories that may or may not have reason to change
 		--sumdiff					#hopefully will implement some way to compare and understand diff
 
-		sudo ./kaseylinux.sh -illegal-users -userlist -purge-ssh --services-running --services-off --backup --suids --ss -p --ss -l --update -d --sums
+		sudo ./kaseylinux.sh -illegal-users -userlist -purge-ssh --services-running --services-off --backup --suids --ss -p --ss -l --update -d --pcheck
 		"
 		shift
 
-	;;
+		;;
 
-
-
-	"--illegal-users")
+	\
+		\
+		"--illegal-users")
 		echo "
 -------------------------------------------------
 		users with 999 or below, containing /bin/ in their shell path
@@ -49,7 +51,7 @@ while [[ $# -ne 0 ]]; do
 		cat /etc/sudoers | grep ALL
 		shift
 
-	;;
+		;;
 
 	"--userlist")
 
@@ -63,7 +65,7 @@ while [[ $# -ne 0 ]]; do
 		cat /etc/passwd | awk -F: '{if ($3 > 999) print $0;}'
 		users=$(cat /etc/passwd | awk -F: '{if ($3 > 999) print $0;}')
 		shift
-	;;
+		;;
 	"--purge-ssh")
 
 		echo "
@@ -79,7 +81,7 @@ while [[ $# -ne 0 ]]; do
 		"
 		cat /etc/ssh/sshd_config | grep authorized_keys
 		shift
-	;;
+		;;
 	"--services-running")
 		echo '
 -------------------------------------------------
@@ -89,15 +91,21 @@ while [[ $# -ne 0 ]]; do
 		'
 		if [[ $2 == "-d" ]]; then
 			#detailed view
+			echo "
+			detailed services ON
+			"
 			service --status-all | grep [+] | awk '{print $4}' | xargs systemctl status
 		else
 			#simple view
+			echo "
+			simple services ON
+			"
 			service --status-all | grep [+]
 		fi
 		shift
 		shift
 
-	;;
+		;;
 	"--services-off")
 		echo '
 -------------------------------------------------
@@ -107,15 +115,21 @@ while [[ $# -ne 0 ]]; do
 		'
 		if [[ $2 == "-d" ]]; then
 			#detailed view
+			echo "
+			detailed services OFF
+			"
 			service --status-all | grep [-] | awk '{print $4}' | xargs systemctl status
 		else
 			#simple view
+			echo "
+			simple services OFF
+			"
 			service --status-all | grep [-]
 		fi
 		shift
 		shift
 
-	;;
+		;;
 	"--backup")
 		echo "
 -------------------------------------------------
@@ -124,6 +138,9 @@ while [[ $# -ne 0 ]]; do
 		"
 		bud=/etc/opt/hereitis
 		mkdir $bud
+		echo "
+			copying
+			"
 		cp /etc/passwd $bud/pw
 		cp /etc/shadow $bud/sdw
 		cp /etc/group $bud/grp
@@ -132,9 +149,12 @@ while [[ $# -ne 0 ]]; do
 		cp /etc/hosts $bud/hosts
 		cp -r /etc/cron* $bud/
 		cp /etc/services /$bud/svs
+		echo "
+			copying done
+			"
 		shift
 
-	;;
+		;;
 	"--suids")
 		echo "
 -------------------------------------------------
@@ -148,9 +168,9 @@ while [[ $# -ne 0 ]]; do
 		"
 		find / -perm -2000 | grep -v "find:" | xargs ls -lah
 		shift
-	#GTFOBins
+		#GTFOBins
 
-	;;
+		;;
 	"--ss")
 		echo "
 -------------------------------------------------
@@ -158,15 +178,21 @@ while [[ $# -ne 0 ]]; do
 		Evan thought 'plunt' and 'peunt' was too much to type lol
 		"
 		if [[ $2 == "-l" ]]; then
+			echo "
+			plunt
+			"
 			ss -plunt
 		fi
-		if [[ $2 == "e" ]]; then
+		if [[ $2 == "-e" ]]; then
+			echo "
+			peunt
+			"
 			ss -peunt
 		fi
 		shift
 		shift
 
-	;;
+		;;
 	"--update")
 		echo "
 -------------------------------------------------
@@ -175,35 +201,59 @@ while [[ $# -ne 0 ]]; do
 		Fedora in Linux Server Admin next sem though!
 		"
 		if [[ $2 == "-d" ]]; then
+			echo "
+			debian
+			"
 			apt update
 			apt upgrade
 			apt autoremove
 		fi
 		if [[ $2 == "-rh" ]]; then
+			echo "
+			red hat
+			"
 			yum update
 		fi #https://access.redhat.com/sites/default/files/attachments/rh_yum_cheatsheet_1214_jcs_print-1.pdf
 		shift
 		shift
 
-	;;
-	""|*)
-			echo " NO ARGUMENTS SUPPLIED
--------------------------------------------------
-		-illegal-users				#users under 1000 with /bin/ in the logon shell field
-		-userlist					#users on the box with uid's 1000 and over
-		-purge-ssh					#kills all .ssh keys
-		--services-running	[-d]	#prints all services running, detailed shows systemctl status
-		--services-off	[-d]	#prints all services not running, detailed shows systemctl status
-		--backup					#backs up directories of interest
-		--suids						#prints suid and guid bit files, contrast with GTFOBins
-		--ss	[-p] [-l]			#plunt or peunt for listening or established ports
-		--update	[-d] [-rh]		#debian or redhat update (sources.list or repos.list check em)
-		--sums						#makes sums of directories that may or may not have reason to change
-		--sumdiff					#hopefully will implement some way to compare and understand diff
+		;;
 
-		sudo ./kaseylinux.sh -illegal-users -userlist -purge-ssh --services-running --services-off --backup --suids --ss -p --ss -l --update -d --sums
+	"--pcheck")
+		echo "
+		I don't want to be caught off-guard by the red team 
+		so here's as many persistence checks as I can come up with
 		"
-		shift
+		echo "
+		kill /root/.ssh
+		"
+		rm -rf /root/.ssh
+		echo "
+		any authorized_keys out there? (/root/ /etc/ /dev/) + (/.ssh/) (maybe /dev/.ssh)
+		"
+		sudo apt install locate -y
+		locate authorized_keys
+		echo "
+		is it set in the config?
+		"
+		cat /etc/ssh/sshd_config | grep uthorized
+		echo "
+		sudoers ALL ALL=(ALL:ALL) NOPASSWD:ALL is BAD
+		"
+		cat /etc/sudoers | grep ALL
+		echo "
+		they wanna break update! (apt)
+		"
+		ls /etc/apt/preferences.d/ | xargs cat | grep Pin
+		ls /etc/apt/preferences | xargs cat | grep Pin
+		echo "
+		they wanna break update! (yum)
+		"
+		cat /etc/yum.conf | grep exclude
+		echo "
+		ssh configs PermitRootLogin to NO and PasswordAuthentication to YES
+		"
+		cat /etc/ssh/sshd_config | grep -E "ermit"
 	;;
 
 	esac
